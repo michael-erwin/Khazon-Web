8e9bef9b9133a43f0612018778bd61c9b8f6d168
Michael Virgines <michael.erwinp@gmail.com>
2018-04-12 00:07:25 +0800

    Maintenance page added.

---
+++ src/components/contents/errors/Maintenance.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="error-code">
+      <i class="icon fa fa-wrench"></i>
+    </h1>
+    <h2 class="error-text">Temporarily Closed</h2>
+    <p class="error-message">Server is currently undergoing maintenance. We will be back soon.</p>
+  </div>
+</template>
+
+<style scoped>
+  .error-message {
+    background-color: rgb(0, 182, 155);
+    color: white;
+  }
+  .error-code {
+    margin-bottom: 15px;
+  }
+</style>
